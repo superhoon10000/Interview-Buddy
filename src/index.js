82a8f9c326678f4bd69c9cfe4045f0a04e419feb
Hoonam Awad <32026971+superhoon10000@users.noreply.github.com>
2026-09-16 19:40:35 -0700

    Revert "Sprint 1: Added Routing"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 import "./styles/index.css";
 import "./styles/layout.css";
 import "./styles/pages.css";
-
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -13,10 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,14 @@
-// Centralized application route paths
+//List of page identifiers, consolidated in one place for easy reference / modification
 export const PAGES = {
-  LOGIN: "/login",
-  REGISTER: "/register",
-  DASHBOARD: "/dashboard",
-
-  INTERVIEW_SETUP: "/interview/setup",
-  INTERVIEW: "/interview/session",
-  SESSION_RESULTS: "/results",
-
-  HISTORY: "/history",
-  LEADERBOARD: "/leaderboard",
-  ANALYTICS: "/analytics",
-
-  SETTINGS: "/settings",
-  CHANGE_PASSWORD: "/settings/password",
-};
+    LOGIN: "login",
+    REGISTER: "register",
+    DASHBOARD: "dashboard",
+    INTERVIEW_SETUP: "interviewSetup",
+    INTERVIEW: "interview",
+    SESSION_RESULTS: "sessionResults",
+    SETTINGS: "settings",
+    CHANGE_PASSWORD: "changePassword",
+    HISTORY: "history",
+    LEADERBOARD: "leaderboard",
+    ANALYTICS: "analytics"
+}
