497da8bfd492f8c99019b3d5763450d681dacb28
Bryan Chavez <Bryan94chavez@gmail.com>
2026-09-23 16:12:40 -0700

    Sprint 2:Made Login look like the mock. Still need to connect the socials and add a css section for logo

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,9 +5,10 @@ function LoginPage({ onLogin, onGoToRegister, loginMessage }) {
     <div className="loginPage">
       <div className="loginCard">
         <div className="loginHeader">
+          <img src="/IBlogo.jpg" alt="Interview Buiddy logo" class="LoginLogo" />
           <h1 className="loginTitle">Interview Buddy</h1>
           <p className="loginSubtitle">
-            Practice interview questions in one place.
+            Technical Interview Excellence
           </p>
         </div>
 
@@ -29,21 +30,44 @@ function LoginPage({ onLogin, onGoToRegister, loginMessage }) {
         )}
 
         <div className="loginForm">
-          <input
-            className="textInput"
-            type="text"
-            placeholder="Username or Email"
-          />
+          <div className="inputGroup">  
+          <label className="inputLabel">Email or Username</label>
+              <input
+                className="textInput"
+                type="text"
+                placeholder="Username or Email"
+              />
+          </div>
 
-          <input
-            className="textInput"
-            type="password"
-            placeholder="Password"
-          />
+
+          <div className="inputGroup">
+            <div className="labelRow"> 
+               <label className="inputLabel">Password</label>
+               <a href="#" className="forgotPasswordLink">Forgot Password</a>
+            </div>
+                 <input
+                   className="textInput"
+                   type="password"
+                   placeholder="Password"
+                   />
+          </div>
 
           <button className="primaryButton" onClick={onLogin}>
-            Login
+            Login to Workspace &rarr;
           </button>
+
+          <div className="divider">
+            <span className="dividerText">OR</span>
+          </div>
+
+          <div className="socialButtons"> 
+            <button className="socialButton" type="button">
+            <span style={{ color: 'gold' }}>&#9679;</span> Google
+            </button>
+            <button className="socialButton" type="button">
+              <span>&diams</span> Github
+            </button>
+          </div>
 
           <button className="textButton" onClick={onGoToRegister}>
             Create Account
